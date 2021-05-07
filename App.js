d3f1de3cb33298550e876844c3cb92fed880a45b
ShinyDanTDM <67851579+ShinyDanTDM@users.noreply.github.com>
2021-05-07 16:28:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Stylesheet, View, Text } from 'react-native';
+import Homescreen from './screens/HomeScreen';
+
+export default class App extends Component{
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <Hozmescreen/>
+      </View>
+    )
+  }
+}
